07adebbadc0b53a07ad96fb6610437f4113b786c
Abdulhakim Fışkın <abdulhakimfiskin@gmail.com>
2021-03-17 16:12:04 +0300

    13 Mart Commit 3

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,18 +9,18 @@ import { Product } from '../models/product';
 })
 export class ProductService {
 
-  apiUrl = "https://localhost:44363/api/";
+  apiUrl = 'https://localhost:44363/api/';
   
   constructor(private httpClient:HttpClient) { }
 
-  getProducts():Observable<ListResponseModel<Product>>{
+  getProducts():Observable<ListResponseModel<Product>> {
     let newPath = this.apiUrl + "products/getall"
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
 
   getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>>{
-    let newPath = this.apiUrl + "products/getbycategory?categoryId=" + categoryId 
-    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+    let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId 
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
   
 
